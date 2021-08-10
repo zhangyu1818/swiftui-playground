c2cff2163cc85f461ea342629e94b31e26babbf4
zhangyu1818 <723156735@qq.com>
2021-08-10 22:15:04 +0800

    Initial Commit

---
+++ SwiftUI Playground/SwiftUI_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PlaygroundApp.swift
+//  SwiftUI Playground
+//
+//  Created by ZhangYu on 2021/8/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
